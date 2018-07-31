e3295d64ac72b9ba42f0a8451dcc68b1d6c02c5e
Alexander Bruy <alexander.bruy@gmail.com>
2018-07-31 09:05:49 +0300

    strings update

--- i18n/qconsolidate_uk.ts
+++ i18n/qconsolidate_uk.ts
@@ -3,158 +3,353 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="aboutdialog.py" line="54"/>
-        <source>Version: %s</source>
+        <location filename="../gui/aboutdialog.py" line="57"/>
+        <source>About {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="aboutdialog.py" line="79"/>
-        <source>&lt;p&gt;Consolidates all layers from current QGIS project into one directory.&lt;/p&gt;
-    &lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: Alexander Bruy&lt;/p&gt;
-    &lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;http://hub.qgis.org/projects/qconsolidate&quot;&gt;http://hub.qgis.org/projects/qconsolidate&lt;/a&gt;&lt;/p&gt;
-    &lt;p&gt;Please report bugs at &lt;a href=&quot;http://hub.qgis.org/projects/qconsolidate/issues&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;
-    </source>
+        <location filename="../gui/aboutdialog.py" line="61"/>
+        <source>Version: {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/aboutdialog.py" line="63"/>
+        <source>&lt;p&gt;{description}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: {developer}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;{homepage}&quot;&gt;{homepage}&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;{bugtracker}&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>ConsolidateThread</name>
+</context>
+<context>
+    <name>CopyWriter</name>
     <message>
-        <location filename="consolidatethread.py" line="171"/>
-        <source>Cannot read file %s:
-%s.</source>
+        <location filename="../writers/copywriter.py" line="50"/>
+        <source>Copy to directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CopyWriterTask</name>
+    <message>
+        <location filename="../writers/copywriter.py" line="118"/>
+        <source>Layers from the &quot;{provider}&quot; provider are currently not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/copywriter.py" line="121"/>
+        <source>Plugin layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/copywriter.py" line="124"/>
+        <source>Mesh layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../writers/copywriter.py" line="139"/>
+        <source>Failed to parse URI: &quot;{uri}&quot;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="consolidatethread.py" line="178"/>
-        <source>Parse error at line %d, column %d:
-%s</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="consolidatethread.py" line="188"/>
-        <source>Cannot write file %s:
-%s.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="consolidatethread.py" line="219"/>
-        <source>Cannot copy layer %s</source>
+        <location filename="../writers/copywriter.py" line="145"/>
+        <source>Not supported GDAL virtual filesystem layer &quot;{layerType}&quot;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="ui/aboutdialogbase.ui" line="14"/>
-        <source>About QConsolidate</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="ui/aboutdialogbase.ui" line="44"/>
+        <location filename="../ui/aboutdialogbase.ui" line="40"/>
         <source>TextLabel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui/aboutdialogbase.ui" line="30"/>
+        <location filename="../ui/aboutdialogbase.ui" line="63"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16pt; font-weight:600;&quot;&gt;QConsolidate&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExportWriter</name>
+    <message>
+        <location filename="../writers/exportwriter.py" line="45"/>
+        <source>Export to directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExportWriterTask</name>
+    <message>
+        <location filename="../writers/exportwriter.py" line="91"/>
+        <source>Layers from the &quot;{provider}&quot; provider are currently not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/exportwriter.py" line="106"/>
+        <source>Plugin layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/exportwriter.py" line="109"/>
+        <source>Mesh layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../ui/directorywriterwidgetbase.ui" line="14"/>
+        <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui/aboutdialogbase.ui" line="67"/>
-        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <location filename="../ui/directorywriterwidgetbase.ui" line="32"/>
+        <source>Vector layers format</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/directorywriterwidgetbase.ui" line="42"/>
+        <source>Group files into subfolders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/directorywriterwidgetbase.ui" line="49"/>
+        <source>Raster layers format</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GeopackageWriter</name>
+    <message>
+        <location filename="../writers/geopackagewriter.py" line="61"/>
+        <source>GeoPackage</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GeopackageWriterTask</name>
+    <message>
+        <location filename="../writers/geopackagewriter.py" line="80"/>
+        <source>&quot;GeoPackage&quot; driver not found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../writers/geopackagewriter.py" line="85"/>
+        <source>Failed to create database: {message}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../writers/geopackagewriter.py" line="118"/>
+        <source>Layers from the &quot;{provider}&quot; provider are currently not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/geopackagewriter.py" line="128"/>
+        <source>Plugin layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/geopackagewriter.py" line="131"/>
+        <source>Mesh layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
     </message>
 </context>
 <context>
     <name>QConsolidate</name>
-    <message>
-        <location filename="qconsolidate.py" line="90"/>
-        <source>QConsolidate</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="78"/>
-        <source>Consolidates all layers from current QGIS project into one directory</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="72"/>
-        <source>QGIS %s detected.
-</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="72"/>
-        <source>This version of QConsolidate requires at least QGIS version 2.0.
-Plugin will not be enabled.</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>QConsolidateDialog</name>
     <message>
-        <location filename="ui/qconsolidatedialogbase.ui" line="14"/>
+        <location filename="../ui/qconsolidatedialogbase.ui" line="14"/>
         <source>QConsolidate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui/qconsolidatedialogbase.ui" line="20"/>
+        <location filename="../ui/qconsolidatedialogbase.ui" line="43"/>
         <source>Output directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui/qconsolidatedialogbase.ui" line="32"/>
-        <source>Browse...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidatedialog.py" line="55"/>
-        <source>Select output directory</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidatedialog.py" line="130"/>
-        <source>QConsolidate: Error</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidatedialog.py" line="67"/>
-        <source>Output directory is not set. Please specify output directory.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidatedialog.py" line="76"/>
+        <location filename="../gui/qconsolidatedialog.py" line="80"/>
         <source>Directory exists</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="76"/>
-        <source>Output directory already contains &apos;layers&apos; subdirectory. Maybe this directory was used to consolidate another project. Continue?</source>
+        <location filename="../ui/qconsolidatedialogbase.ui" line="36"/>
+        <source>Consolidation method</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="86"/>
-        <source>Can&apos;t create directory for layers.</source>
+        <location filename="../ui/qconsolidatedialogbase.ui" line="66"/>
+        <source>Export layers from remote datasources</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="106"/>
-        <source>Cancel</source>
+        <location filename="../gui/qconsolidatedialog.py" line="51"/>
+        <source>Select directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="148"/>
-        <source>Close</source>
+        <location filename="../gui/qconsolidatedialog.py" line="80"/>
+        <source>Destination directory already has &quot;layers&quot; subdirectory. Probably it already contains data from another project. Continue?</source>
         <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QConsolidatePlugin</name>
+    <message>
+        <location filename="../qconsolidateplugin.py" line="97"/>
+        <source>QConsolidate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qconsolidateplugin.py" line="59"/>
+        <source>About QConsolidate&#xe2;&#x80;&#xa6;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qconsolidateplugin.py" line="94"/>
+        <source>Project consolidated successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SpatialiteWriter</name>
+    <message>
+        <location filename="../writers/spatialitewriter.py" line="61"/>
+        <source>SpatiaLite</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SpatialiteWriterTask</name>
+    <message>
+        <location filename="../writers/spatialitewriter.py" line="80"/>
+        <source>&quot;SQLite&quot; driver not found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../writers/spatialitewriter.py" line="85"/>
+        <source>Failed to create database: {message}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../writers/spatialitewriter.py" line="100"/>
+        <source>Layers from the &quot;{provider}&quot; provider are currently not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/spatialitewriter.py" line="111"/>
+        <source>Raster layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/spatialitewriter.py" line="131"/>
+        <source>Plugin layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/spatialitewriter.py" line="134"/>
+        <source>Mesh layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>WriterBase</name>
+    <message>
+        <location filename="../writers/writerbase.py" line="63"/>
+        <source>Base</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WriterTaskBase</name>
+    <message>
+        <location filename="../writers/writerbase.py" line="232"/>
+        <source>Failed to export layer &quot;{layer}&quot;: {message}.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../writers/writerbase.py" line="206"/>
+        <source>Failed to export layer &quot;{layer}&quot;: Cannot set pipe provider.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../writers/writerbase.py" line="220"/>
+        <source>Failed to export layer &quot;{layer}&quot;: Cannot set pipe nuller.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ZipWriter</name>
+    <message>
+        <location filename="../writers/zipwriter.py" line="63"/>
+        <source>ZIP archive</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ZipWriterTask</name>
+    <message numerus="yes">
+        <location filename="../writers/zipwriter.py" line="88"/>
+        <source>Failed to zip layers.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../writers/zipwriter.py" line="128"/>
+        <source>Layers from the &quot;{provider}&quot; provider are currently not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/zipwriter.py" line="142"/>
+        <source>Plugin layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../writers/zipwriter.py" line="145"/>
+        <source>Mesh layers are currently not supported.</source>
+        <comment>QConsolidate</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
     </message>
 </context>
 </TS>
